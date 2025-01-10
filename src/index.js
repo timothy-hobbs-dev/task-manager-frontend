cbccd110c7c1a0f77b4467b53a2c26a45c9abc0a
timothy-kumah <timothy.kumah@amalitech.com>
2025-01-10 08:06:06 +0000

    used correct redirect uri

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { AuthProvider } from "react-oidc-context";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_DaEPtiEmo",
   client_id: "4dnc8h177ghqf6tb9u7d33uk68",
-  redirect_uri: "http://localhost:3000/dashboard",
+  redirect_uri: "https://master.d388ak6c2ywigz.amplifyapp.com/dashboard",
   response_type: "code",
   scope: "aws.cognito.signin.user.admin email openid phone profile",
 };
